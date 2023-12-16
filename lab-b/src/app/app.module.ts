9efdeed4baa4b58d9cb70b1d5a6aa7dfe6973134
hj46525 <kuba@hubkiewicz.pl>
2023-12-16 10:33:12 +0100

    update

--- lab-b/src/app/app.module.ts
+++ lab-b/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RandomComponent } from './random/random.component';
 import { ListComponent } from './list/list.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { ListComponent } from './list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
