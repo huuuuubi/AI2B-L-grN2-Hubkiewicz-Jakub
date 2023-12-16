feb571fe439abf14e02800588865c0070828b7e6
hj46525 <kuba@hubkiewicz.pl>
2023-12-16 11:19:30 +0100

    up

--- lab-c/src/app/app.module.ts
+++ lab-c/src/app/app.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailsComponent,
+    AddPersonComponent,
+    NotFoundComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
